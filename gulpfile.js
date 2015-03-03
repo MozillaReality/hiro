91aa61552e19c83bf050d394156673c2e0213e4b
Casey Yee <caseyyee.ca@gmail.com>
2015-03-03 13:28:31 -0800

    express server, tweets using io

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,11 @@ var gulp = require('gulp');
 var sketch = require('gulp-sketch');
 var connect = require('gulp-connect');
 var clean = require('gulp-rimraf');
+var Twit = require('twit')
 
+var serverPort = 8080;
+
+var T = new Twit(require('./appKeys/twitter').getKeys());
 
 gulp.task('clean', function(cb) {
   gulp.src('./s23/images/*').pipe(clean())
@@ -27,4 +31,39 @@ gulp.task('connect', function() {
   connect.server();
 });
 
-gulp.task('default', ['connect']);
+gulp.task('express', function() {
+  var express = require('express');
+  var app = express();
+  app.use(express.static(__dirname));
+
+  var tweets = [];
+  var cacheTweets = 15;
+
+  var stream = T.stream('statuses/filter', { track: '#GDC2015' })
+
+  stream.on('tweet', function (tweet) {
+    io.sockets.emit('tweet', {
+      user: tweet.user.screen_name,
+      text: tweet.text
+    });
+
+
+    if (tweets.length > cacheTweets) {
+      tweets.shift();
+    }
+
+    tweets.push(tweet);
+  });
+
+  app.get('/tweets', function(req, res) {
+    res.send(tweets);
+  })
+
+  var server = app.listen(serverPort);
+
+  console.log('Listening on port ' + serverPort);
+
+  var io = require('socket.io').listen(server);
+})
+
+gulp.task('default', ['express']);
