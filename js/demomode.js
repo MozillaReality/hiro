75ad2849e34630379172435574ef8f86ee78f601
Casey Yee <caseyyee.ca@gmail.com>
2015-03-04 02:16:25 -0800

    kb mode, adjusted demo timing

--- js/demomode.js
+++ js/demomode.js
@@ -8,7 +8,7 @@ var VRDemo = (function() {
 	}
 
 	VRDemo.prototype.start = function() {
-		var startDelay = 30*1000;
+		var startDelay = 60*1000;
 		var self = this;
 
 		if (this.currentIndex >= DemoScript.length) {
